50ef131cae4fdfdf93126bf7a8a53e62fc042dbc
TinTin <tintin200227@gmail.com>
2021-03-01 17:38:29 +0300

    Initial commit

---
+++ app/src/main/java/com/example/tinapp/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.tinapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+    fun onClickTest(view : View){
+        val myText = findViewById<TextView>(R.id.myText)
+        myText.text = "testing"
+    }
+}
