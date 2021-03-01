e8d79596df91c6b5cb76b3db433ae474d8d0fbee
TinTin <tintin200227@gmail.com>
2021-03-01 18:06:42 +0300

    new fun in inClickTest

--- app/src/main/java/com/example/tinapp/MainActivity.kt
+++ app/src/main/java/com/example/tinapp/MainActivity.kt
@@ -6,15 +6,17 @@ import android.view.View
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private var num:Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
     }
 
-    fun onClickTest(view : View){
+    fun onClickTest(view : View) {
         val myText = findViewById<TextView>(R.id.myText)
-        myText.text = "testing"
-    }
+        if (num % 2 == 0) myText.text = "First Test" else myText.text = "Second Test"
+        num++
+     }
     //тестовая запись для проверки Git
 }
